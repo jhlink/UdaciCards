8ce9fbcffd5b569ca9b8eaaeb32b87a0f35e85c1
James Kim <jhlink@users.noreply.github.com>
2018-06-26 15:19:42 -0400

    feat: Create DeckList component while showcase dummy data

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React, { Component }  from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Deck from './components/Deck';
+import DeckList from './components/DeckList';
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Deck />
+        <View style={{ height: 20 }}/>
+        <DeckList />
       </View>
     );
   }
@@ -16,7 +17,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
---
+++ components/DeckList.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import styled from 'styled-components/native';
+import Deck from './Deck';
+
+const CenterView = styled.View`
+  justify-content: center;
+  borderBottomWidth: 1px;
+  borderBottomColor: gray;
+  borderStyle: solid;
+`;
+
+const dummyData = [
+  { 
+    deckName: 'Italian',
+    cardCount: 30 
+  },
+  { 
+    deckName: 'English',
+    cardCount: 20 
+  }
+];
+
+  
+
+class DeckList extends Component { 
+  state = {
+    decks: []
+  }
+
+  componentDidMount() {
+    this.setState({ decks: dummyData });  
+  }
+
+  render() {
+    const { decks } = this.state;
+
+    return (
+      <View>
+        { decks.map((deck) => {
+          return (
+            <CenterView key={deck.deckName}>
+              <Deck deckName={ deck.deckName } cardCount={ deck.cardCount } />
+            </CenterView>
+          );
+        })}
+      </View>
+    ); 
+  }
+}
+
+export default DeckList;
+
