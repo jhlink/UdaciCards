a89ceaa9efe4eb455f9ca58fb47ac498b96db303
James Kim <jhlink@users.noreply.github.com>
2018-06-26 19:53:24 -0400

    feat: Create utils/color file for handling static color values

---
+++ utils/colors.js
@@ -0,0 +1,10 @@
+// utils/colors.js
+
+export const purple = '#292477';
+export const gray = '#757575';
+export const white = '#FFF';
+export const red = '#B71845';
+export const orange = '#F26F28';
+export const blue = '#4E4CB8';
+export const lightPurple = '#7C53C3';
+export const pink = '#B93FB3';
