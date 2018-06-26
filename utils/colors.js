1c6803883531bd81c8a4703b74b378a35990c09c
James Kim <jhlink@users.noreply.github.com>
2018-06-26 19:55:04 -0400

    fix: Add black color to utils/colors file

--- utils/colors.js
+++ utils/colors.js
@@ -3,6 +3,7 @@
 export const purple = '#292477';
 export const gray = '#757575';
 export const white = '#FFF';
+export const black = '#000';
 export const red = '#B71845';
 export const orange = '#F26F28';
 export const blue = '#4E4CB8';
