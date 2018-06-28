4a7a4df749a8f4c502dee61edefa105a6efc8314
James Kim <jhlink@users.noreply.github.com>
2018-06-28 04:24:49 -0400

    feat: Add title for primary page loaded on app startup

--- components/MainNavigator.js
+++ components/MainNavigator.js
@@ -3,8 +3,11 @@ import { createStackNavigator } from 'react-navigation';
 import DeckListView from './DeckListView';
 
 const MainNavigator =  createStackNavigator({
-  Home: {
-    screen: DeckListView
+  Decks: {
+    screen: DeckListView,
+    navigationOptions: {
+      title: 'Decks'
+    }
   }
 });
 
