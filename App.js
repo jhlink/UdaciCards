d025ae4bada69c0aa32ecc948575c2c64ae77d4d
James Kim <jhlink@users.noreply.github.com>
2018-06-28 20:42:01 -0400

    feat: Add Provider to root App component

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import React, { Component }  from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { StyleSheet, View } from 'react-native';
 import MainNavigator from './components/MainNavigator';
 import AppStatusBar from './components/AppStatusBar';
 import { white } from './utils/colors'; 
+import reducer from './reducers'
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <AppStatusBar /> 
-        <MainNavigator />
-      </View>
+      <Provider store={ createStore( reducer ) } >
+        <View style={styles.container}>
+          <AppStatusBar /> 
+          <MainNavigator />
+        </View>
+      </Provider>
     );
   }
 }
