78fbce241bfd04ba5a1e00ea65eff7789f334e87
James Kim <jhlink@users.noreply.github.com>
2018-06-30 16:59:01 -0400

    feat: Integrate local notification into root App component

--- App.js
+++ App.js
@@ -6,14 +6,38 @@ import MainNavigator from './components/MainNavigator';
 import AppStatusBar from './components/AppStatusBar';
 import { white } from './utils/colors'; 
 import reducer from './reducers';
+import { checkQuizCount, setQuizCount, clearQuizCount } from './utils/notifHelper';
+import TextButton from './components/TextButton';
 
 export default class App extends Component {
+  componentDidMount() {
+    checkQuizCount();    
+  }
+
+  tagUr = () => {
+    clearQuizCount();
+  }
+
+  tagMur = () => {
+    setQuizCount(1); 
+  }
+
   render() {
     return (
       <Provider store={ createStore( reducer ) } >
         <View style={styles.container}>
           <AppStatusBar /> 
           <MainNavigator />
+          <TextButton
+            onPress={this.tagUr}
+          >
+         get 
+          </TextButton>
+          <TextButton
+            onPress={this.tagMur}
+          >
+         set 
+          </TextButton>
         </View>
       </Provider>
     );
