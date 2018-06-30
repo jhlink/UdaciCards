336d70bba6c6711a9a97bf01df436e4ac3a82cff
James Kim <jhlink@users.noreply.github.com>
2018-06-30 19:13:57 -0400

    feat: Create constants file storageKeys w/ PARTIAL_DESK_KEY

---
+++ src/utils/storageKeys.js
@@ -0,0 +1,3 @@
+// Note: Each deck is assigned a unique storage key, which must be appended to the partial key below before usage with AsyncStorage.
+export const PARTIAL_DECK_KEY = 'UdaciCards:Deck:'; 
+
