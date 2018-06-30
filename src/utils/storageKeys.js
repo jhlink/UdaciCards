99c8302cef1ea00456bb0c7cbd5bccdfe3b4257a
James Kim <jhlink@users.noreply.github.com>
2018-06-30 19:42:34 -0400

    fix: Shorten DeckStorage key

--- src/utils/storageKeys.js
+++ src/utils/storageKeys.js
@@ -1,3 +1,3 @@
 // Note: Each deck is assigned a unique storage key, which must be appended to the partial key below before usage with AsyncStorage.
-export const PARTIAL_DECK_KEY = 'UdaciCards:Deck:'; 
+export const PARTIAL_DECK_KEY = 'Udi:Deck:'; 
 
