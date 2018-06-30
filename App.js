2b209f538ea1107d51964b568dbbd70a223038ee
James Kim <jhlink@users.noreply.github.com>
2018-06-30 17:05:10 -0400

    style: Organize imports in App component

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React, { Component }  from 'react';
+import { StyleSheet, View } from 'react-native';
+
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { StyleSheet, View } from 'react-native';
+
+import reducer from './reducers';
 import MainNavigator from './components/MainNavigator';
 import AppStatusBar from './components/AppStatusBar';
 import { white } from './utils/colors'; 
-import reducer from './reducers';
 
 export default class App extends Component {
   render() {
