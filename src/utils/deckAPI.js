c997a61b72af2d7f0f35695e293064841030c5fb
James Kim <jhlink@users.noreply.github.com>
2018-06-30 19:42:48 -0400

    feat: Create deckAPI helper with getAllDecks function

---
+++ src/utils/deckAPI.js
@@ -0,0 +1,22 @@
+import { AsyncStorage } from 'react-native';
+import { PARTIAL_DECK_KEY } from './storageKey';
+import uuidv4 from 'uuid/v4';
+
+export function getAllDecks() {
+  return AsyncStorage.getAllKeys((err, keys) => {
+    const deckKeys = keys.filter( (key) => key.includes( PARTIAL_DECK_KEY ) );
+    return AsyncStorage.multiGet(deckKeys, (err, stores) => {
+      return stores.map( (arrayOfDeckKeyAndValue) => {
+        const deckKey = arrayOfDeckKeyAndValue[0];
+        const deckValue = JSON.parse(arrayOfDeckKeyAndValue[1]);
+           
+        console.log( deckKey );
+        console.log( deckValue );
+        return { [ deckKey ] : deckValue };
+      });
+    });
+  });
+}
+
+        
+
