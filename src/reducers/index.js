92782b77f3cea245fe843c56062e1446e5785ecc
James Kim <jhlink@users.noreply.github.com>
2018-06-30 21:23:50 -0400

    feat: Implement deckReducer using combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,29 @@
-export default function entries(state = {}, action) {
-  return state;
+import { combineReducers } from 'redux';
+import {
+  GET_DECKS,
+  ADD_DECK,
+  REMOVE_DECK
+} from '../actions';
+
+
+function deckReducer (state = {}, action) {
+  const { decks, deck, deckId } = action;
+
+  switch (action.type) {
+    case GET_DECKS: {
+      return { 
+        ...state,
+        decks 
+      };
+    }
+
+    default: 
+      return state;
+  }
 }
 
+const rootReducer = combineReducers({
+  deckReducer,
+});
+
+export default rootReducer;
