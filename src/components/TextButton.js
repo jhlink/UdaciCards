268589dd92dd9770b0f067b2886d0631fda459ae
James Kim <jhlink@users.noreply.github.com>
2018-07-01 14:51:38 -0400

    fix: Change TextButton into arrow function

--- src/components/TextButton.js
+++ src/components/TextButton.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Text, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function TextButton ({ children, onPress, style = {} }) {
+const TextButton = ({ children, onPress, style = {} }) => {
   return (
     <TouchableOpacity
-      onPress={onPress}>
+      onPress={ onPress }>
       <Text style={[styles.btn, style]}>{ children }</Text> 
     </TouchableOpacity>
   );
-}
+};
 
 const styles = StyleSheet.create({
   btn: {
@@ -23,3 +23,5 @@ const styles = StyleSheet.create({
     borderWidth: 2,
   }
 });
+
+export default TextButton;
