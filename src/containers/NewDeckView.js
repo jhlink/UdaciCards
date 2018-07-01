c8e3be562bf0bdf715e009888dce60e6cdab24c9
James Kim <jhlink@users.noreply.github.com>
2018-07-01 05:41:53 -0400

    feat: Implement addDeck feature with redux support in NewDeskView container

--- src/containers/NewDeckView.js
+++ src/containers/NewDeckView.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { addDeck } from '../actions';
+import { createDeck } from '../actions';
 import NewDeckForm from '../components/NewDeckForm';
+import uuidv4 from 'uuid/v4';
 
+const INIT_DECK = {
+  deckName: '',
+  deckAdded: false
+};
 
 class NewDeckView extends Component { 
   state = {
-    deckName: 'Deck'
+    deckName: '',
+    deckAdded: false
   }
 
   toSingleDeckView = () => {
-    this.props.navigation.navigate(
-      'DeckView',
-      { ...this.state }
-    );
+    // TODO: Will replace 'test' with uuidv4 post development.
+    const newDeck = {
+      cardCount: 0,
+      id: 'test',
+      ...this.state
+    };
+
+    this.props.addDeck(newDeck);
+  }
+
+  componentDidUpdate( ) {
+    if (this.props.deckAdded) {
+      this.setState = INIT_DECK; 
+
+      this.props.goToDeck(
+        'DeckView',
+        { ...this.state }
+      );
+    }
   }
 
   handleDeckNameChange = (deckName) => {
@@ -34,5 +55,24 @@ class NewDeckView extends Component {
   }
 }
 
-export default connect()(NewDeckView);
+function mapStateToProps ( state ) {
+  const { deck } = state.deckReducer;
+
+  console.log(deck);
+
+  return {
+    ...deck
+  };
+}
+
+function mapDispatchToProps ( dispatch, { navigation }) {
+  return {
+    addDeck: (deckData) =>  dispatch(createDeck(deckData)),
+    goToDeck: (componentAsString, propsToPass) => {
+      navigation.navigate(componentAsString, propsToPass);      
+    }
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewDeckView);
 
