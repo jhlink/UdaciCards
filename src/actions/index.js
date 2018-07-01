ec98a1e74bf137eda8d65199dce28fe147c5835c
James Kim <jhlink@users.noreply.github.com>
2018-06-30 20:13:40 -0400

    feat: Implement getDecks thunk'

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import * as DeckAPI from '../utils/deckAPI';
+
 // Actions
 export const GET_DECKS = 'GET_DECKS';
 
@@ -10,3 +12,8 @@ const loadDecks = ( decks ) => {
 };
 
 // Thunks
+export const getDecks = () => dispatch => {
+  return DeckAPI.getAllDecks()
+    .then( (decks) => dispatch(loadDecks(decks)));
+};
+   
