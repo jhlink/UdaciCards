08fd84a48abcc61121936208ad0c81e41b8d7abc
James Kim <jhlink@users.noreply.github.com>
2018-06-30 22:37:44 -0400

    fix: Return Promise result of AsyncStorage of all deckAPI helper arrow functions

--- src/utils/deckAPI.js
+++ src/utils/deckAPI.js
@@ -2,7 +2,7 @@ import { AsyncStorage } from 'react-native';
 import { PARTIAL_DECK_KEY } from './storageKeys';
 
 export const getAllDecks = () => {
-  AsyncStorage.getAllKeys((err, keys) => {
+  return AsyncStorage.getAllKeys((err, keys) => {
     const deckKeys = keys.filter( (key) => key.includes( PARTIAL_DECK_KEY ) );
     return AsyncStorage.multiGet(deckKeys, (err, stores) => {
       return stores.map( (arrayOfDeckKeyAndValue) => {
@@ -21,7 +21,7 @@ export const getAllDecks = () => {
 export const addDeck = ( deckId, deckJSONData ) => {
   const deckAsyncStorageKey = PARTIAL_DECK_KEY + deckId;
   const serializedData = JSON.stringify( deckJSONData );
-  AsyncStorage.setItem(deckAsyncStorageKey, serializedData)
+  return AsyncStorage.setItem(deckAsyncStorageKey, serializedData)
     .then( (err) => {
       if (err) {
         console.error('addDeck storage failure');
@@ -34,7 +34,7 @@ export const addDeck = ( deckId, deckJSONData ) => {
 //  Note: `deckId` is assumed to be a string.
 export const deleteDeck = ( deckId ) => {
   const deckAsyncStorageKey = PARTIAL_DECK_KEY + deckId;
-  AsyncStorage.removeItem(deckAsyncStorageKey)
+  return AsyncStorage.removeItem(deckAsyncStorageKey)
     .then( (err) => {
       if (err) {
         console.error('deleteDeck storage failure');
