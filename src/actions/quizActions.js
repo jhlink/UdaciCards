1ade04bd28b7cc70d4e29fd804b8f6baa7402801
James Kim <jhlink@users.noreply.github.com>
2018-07-02 19:51:22 -0400

    feat: Create QuizActions

---
+++ src/actions/quizActions.js
@@ -0,0 +1,7 @@
+import * as QuizAPI from '../utils/quizAPI';
+
+// Actions
+export const GET_QUIZ_COUNT = 'GET_QUIZ_COUNT';
+export const INCREMENT_QUIZ_COUNT = 'INCREMENT_QUIZ_COUNT';
+export const CLEAR_QUIZ_COUNT = 'CLEAR_QUIZ_COUNT';
+
