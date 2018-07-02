5695e1fe30b37b550904bb0c8dbba0c3b723361c
James Kim <jhlink@users.noreply.github.com>
2018-07-02 19:53:06 -0400

    feat: Create quiz action creators

--- src/actions/quizActions.js
+++ src/actions/quizActions.js
@@ -5,3 +5,23 @@ export const GET_QUIZ_COUNT = 'GET_QUIZ_COUNT';
 export const INCREMENT_QUIZ_COUNT = 'INCREMENT_QUIZ_COUNT';
 export const CLEAR_QUIZ_COUNT = 'CLEAR_QUIZ_COUNT';
 
+// Action Creators
+const getQuizCount = ( count ) => {
+  return {
+    type: GET_QUIZ_COUNT,
+    count
+  };
+};
+
+const incrementQuizCount = ( ) => {
+  return {
+    type: INCREMENT_QUIZ_COUNT,
+  };
+};
+
+const clearQuizCount = ( ) => {
+  return {
+    type: CLEAR_QUIZ_COUNT,
+  };
+};
+
