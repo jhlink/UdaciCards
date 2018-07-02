cf8f023924f9aacd5a6a8882b1b94013ab5115bb
James Kim <jhlink@users.noreply.github.com>
2018-07-02 19:49:16 -0400

    refactor: Split quizAPI from notificationAPI

--- src/utils/notificationAPI.js
+++ src/utils/notificationAPI.js
@@ -2,34 +2,7 @@ import {
   AsyncStorage,
 } from 'react-native';
 import { Notifications, Permissions } from 'expo';
-import { NOTIFICATION_KEY, QUIZ_COUNT_KEY } from './storageKeys'; 
-
-export const setQuizCount = async (quizCount) => {
-  try {
-    await AsyncStorage.setItem(QUIZ_COUNT_KEY, JSON.stringify(quizCount));
-  } catch (error) {
-    console.log('Error setQuizCount: ', error);
-  }
-};
-
-export const clearQuizCount = async () => {
-  try {
-    await AsyncStorage.removeItem(QUIZ_COUNT_KEY);
-  } catch (error) {
-    console.log('Error clearQuizCount: ', error);
-  }
-};
-
-export const getQuizCount = async () => {
-  try {
-    const quizCount = await AsyncStorage.getItem(QUIZ_COUNT_KEY);
-    const parsedResult = JSON.parse(quizCount); 
-
-    return parsedResult;
-  } catch (error) {
-    console.log('Error getQuizCount: ', error);
-  }
-};
+import { NOTIFICATION_KEY } from './storageKeys'; 
 
 export const clearLocalNotification = async () => {
   try {
---
+++ src/utils/quizAPI.js
@@ -0,0 +1,31 @@
+import {
+  AsyncStorage,
+} from 'react-native';
+import { QUIZ_COUNT_KEY } from './storageKeys'; 
+
+export const setQuizCount = async (quizCount) => {
+  try {
+    await AsyncStorage.setItem(QUIZ_COUNT_KEY, JSON.stringify(quizCount));
+  } catch (error) {
+    console.log('Error setQuizCount: ', error);
+  }
+};
+
+export const clearQuizCount = async () => {
+  try {
+    await AsyncStorage.removeItem(QUIZ_COUNT_KEY);
+  } catch (error) {
+    console.log('Error clearQuizCount: ', error);
+  }
+};
+
+export const getQuizCount = async () => {
+  try {
+    const quizCount = await AsyncStorage.getItem(QUIZ_COUNT_KEY);
+    const parsedResult = JSON.parse(quizCount); 
+
+    return parsedResult;
+  } catch (error) {
+    console.log('Error getQuizCount: ', error);
+  }
+};
