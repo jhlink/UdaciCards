ce9a8525cc0f067e744282cb3e419525da898a87
James Kim <jhlink@users.noreply.github.com>
2018-07-03 02:30:22 -0400

    feat: Implement notification feature based on quiz count

--- src/containers/AppLogic.js
+++ src/containers/AppLogic.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import AppContainer from '../components/AppContainer';
 import { purgeQuizCount, fetchQuizCount, setNotificationFlag, clearNotificationFlag } from '../actions';
 
-export default class AppLogic extends Component {
+class AppLogic extends Component {
+  state = {
+    setFlag: true,
+  }
+
   componentDidMount() {
-   
+    this.props.dispatch(setNotificationFlag());
+    this.props.dispatch(fetchQuizCount()); 
+
+    //  TEST_POINT
+    //  To test the notification feature, uncomment the line below and reload the app.
+    //    Doing so should result with a count of 0, setting the notification flag
+    //    as seen in the code in utils/notificationAPI.js
+    //this.props.dispatch(purgeQuizCount()); 
+  }
+
+  componentDidUpdate() {
+    const { count } = this.props;
+    const { setFlag } = this.state;
+    if ( count > 0 && setFlag ) {
+      this.props.dispatch(clearNotificationFlag());   
+      this.setState({ setFlag: false });
+    }
   }
 
   render() {
@@ -13,3 +34,11 @@ export default class AppLogic extends Component {
     );
   }
 }
+
+function mapStateToProps( state ) {
+  const { count } = state.quizReducer;
+  
+  return { count };
+}
+
+export default connect(mapStateToProps)(AppLogic);
