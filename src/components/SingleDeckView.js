e39eca8500b0810db3f8e0f5161ccf72bd8c0253
James Kim <jhlink@users.noreply.github.com>
2018-07-03 04:05:41 -0400

    style: Apply layout styling in SingleDeckView

--- src/components/SingleDeckView.js
+++ src/components/SingleDeckView.js
@@ -6,14 +6,17 @@ import TextButton from './TextButton';
 import { white, black, darkGray, lightGray } from '../utils/colors';
 
 const CenterView = styled.View`
+  flex: 1;
   align-content: center;
-  justify-content: space-between;
+  justify-content: flex-start;
+  margin-top: 40;
 `;
 
 const ButtonView = styled.View`
-  paddingTop: 200;
-  justify-content: center;
+  flex-direction: row;
+  justify-content: space-evenly;
   align-items: center;
+  marginTop: 40;
 `;
 
 const styles = StyleSheet.create({
@@ -25,6 +28,9 @@ const styles = StyleSheet.create({
     backgroundColor: darkGray,
     color: lightGray,
     borderWidth: 0
+  },
+  deck: {
+    marginBottom: 0,
   }
 });
 
@@ -33,7 +39,11 @@ const SingleDeckView = ({ deckName, cardCount, handleCardAdd, handleQuizStart })
   const quizBtnStyle = disableQuiz ? styles.disabled : styles.strtBtn;
   return (
     <CenterView>
-      <Deck deckName={ deckName } cardCount={ cardCount } />
+      <Deck 
+        deckName={ deckName } 
+        cardCount={ cardCount } 
+        style={ styles.deck }
+      />
       <ButtonView>
         <TextButton
           onPress={ handleCardAdd }
