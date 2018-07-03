c76a558f4f859069055783f08778a8b7e34434ea
James Kim <jhlink@users.noreply.github.com>
2018-07-02 20:14:13 -0400

    fix: Add quizReducer to reducer index.js"

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import questionReducer from './questionReducer';
 import deckReducer from './deckReducer';
+import quizReducer from './quizReducer';
 
 const rootReducer = combineReducers({
   deckReducer,
-  questionReducer
+  questionReducer,
+  quizReducer
 });
 
 export default rootReducer;
