a046474f4acb0083de0334294e3500131ee61bfb
James Kim <jhlink@users.noreply.github.com>
2018-07-02 20:16:21 -0400

    feat: Implement notification actoins

---
+++ src/actions/notificationActions.js
@@ -0,0 +1,6 @@
+import * as NotificationAPI from '../utils/NotificationAPI';
+
+// Actions
+export const CLEAR_NOTIFICATION = 'CLEAR_NOTIFICATION';
+export const SET_NOTIFICATION = 'SET_NOTIFICATION';
+
