bc22c88ac0a89ae4080c0975c0b4f524d6a8d75c
James Kim <jhlink@users.noreply.github.com>
2018-07-02 20:35:03 -0400

    feat: Add notificationReducer to root reducer file"

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import questionReducer from './questionReducer';
 import deckReducer from './deckReducer';
 import quizReducer from './quizReducer';
+import notificationReducer from './notificationReducer';
 
 const rootReducer = combineReducers({
   deckReducer,
   questionReducer,
-  quizReducer
+  quizReducer,
+  notificationReducer,
 });
 
 export default rootReducer;
