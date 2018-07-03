3766949273536ffedb31fd022f0d28e349c9e8c6
James Kim <jhlink@users.noreply.github.com>
2018-07-03 05:42:03 -0400

    feat: Add trash icon associated styling for FlatList items

--- src/components/DeckListView.js
+++ src/components/DeckListView.js
@@ -3,13 +3,15 @@ import {
   TouchableOpacity, 
   View,
   FlatList,
-  StyleSheet
+  StyleSheet,
+
 } from 'react-native';
 import styled from 'styled-components/native';
 import Deck from './Deck';
 import { gray } from '../utils/colors';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const DeckListView = ({ decks, handleDeckNav }) => { 
+const DeckListView = ({ decks, handleDeckNav, handleDeckDelete }) => { 
   return (
     <View>
       <FlatList
@@ -17,14 +19,23 @@ const DeckListView = ({ decks, handleDeckNav }) => {
         keyExtractor={( item ) => item.id}
         renderItem={ ({ item }) => {
           return ( 
-            <TouchableOpacity 
-              style={ styles.item }
-              onPress={ () => handleDeckNav(item)}>
-              <Deck 
-                deckName={ item.deckName } 
-                cardCount={ item.cardCount } 
+            <View style={ styles.itemContainer }>  
+              <TouchableOpacity 
+                style={ styles.item }
+                onPress={ () => handleDeckNav(item)}>
+                <Deck 
+                  styles={ styles.deck }
+                  deckName={ item.deckName } 
+                  cardCount={ item.cardCount } 
+                />
+              </TouchableOpacity>
+              <Icon
+                name="trash"
+                size={50}
+                style={ styles.icon }
+                onPress={ () => console.log('Yay') }
               />
-            </TouchableOpacity>
+            </View>
           ); 
         }}
       />
@@ -34,12 +45,27 @@ const DeckListView = ({ decks, handleDeckNav }) => {
 
 const styles = StyleSheet.create({
   item: {
+    marginLeft: 25,
     paddingTop: 10,
     paddingBottom: 10,
     justifyContent: 'center',
+  },
+  itemContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     borderStyle: 'solid',
     borderBottomWidth: 1,
     borderBottomColor: gray 
+  },
+  icon: {
+    alignSelf: 'center',
+    marginRight: 25,
+  }, 
+  deck: {
+    textAlign: 'left',
+    maxWidth: 300,
+    overflow: 'hidden'
   }
 });
 
